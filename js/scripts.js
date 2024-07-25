56021972aa848bbf1c7e26dfe8b56d6e44a54199
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-07-25 15:38:54 +0200

    migliorati commenti

--- js/scripts.js
+++ js/scripts.js
@@ -3,37 +3,43 @@ const container = document.getElementById('Member-info-container');
 const OurTeam = 
 [
     {
+        //prima persona 
         name : 'Wayne Barnett',
         role : 'Founder & CEO',
         image : 'wayne-barnett-founder-ceo.jpg'
     },
     {
+        //seconda persona 
         name : 'Angela Caroll',
         role : 'Chief Editor',
         image : 'angela-caroll-chief-editor.jpg'
     },
     {
+        //terza persona 
         name : 'Walter Gordon',
         role : 'Office Manager',
         image : 'walter-gordon-office-manager.jpg'
     },
     {
+        //quarta persona 
         name : 'Angela Lopez',
         role : 'Social Media Manager',
         image : 'angela-lopez-social-media-manager.jpg'
     },
     {
+        //quinta persona 
         name : 'Scott Estrada',
         role : 'Developer',
         image : 'scott-estrada-developer.jpg'
     },
     {
+        //sesta persona 
         name : 'Barbara Ramos',
         role : 'Graphic Designer',
         image : 'barbara-ramos-graphic-designer.jpg'
     }
 ];
-//stampo in console i membri del team con nome/ruolo/immagine
+//stampo in console i membri del team con nome/ruolo/immagine E li inserisco nel HTML/DOM
 OurTeam.forEach(function(membri) {
     console.log("Name: " + membri.name + ", Role: " + membri.role + ", Image: " + membri.image);
     memberInfo = document.createElement('p');
