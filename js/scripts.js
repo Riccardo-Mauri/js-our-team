b6f5a53c1a07817a9329625c72a5af90ea31edef
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-07-25 15:18:56 +0200

    creat array di oggetti contenente i nomi ecc dei membri e stamapto in console

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,40 @@
 console.log('ciao mondo');
+
+//creo un array di oggetti per inserire i dati del Team
+const OurTeam = 
+[
+    {
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        image : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name : 'Angela Caroll',
+        role : 'Chief Editor',
+        image : 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name : 'Walter Gordon',
+        role : 'Office Manager',
+        image : 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name : 'Angela Lopez',
+        role : 'Social Media Manager',
+        image : 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name : 'Scott Estrada',
+        role : 'Developer',
+        image : 'scott-estrada-developer.jpg'
+    },
+    {
+        name : 'Barbara Ramos',
+        role : 'Graphic Designer',
+        image : 'barbara-ramos-graphic-designer.jpg'
+    }
+];
+//stampo in console i mebri del team con nome/ruolo/immagine
+OurTeam.forEach(function(membri) {
+    console.log("Name: " + membri.name + ", Role: " + membri.role + ", Image: " + membri.image);
+});
